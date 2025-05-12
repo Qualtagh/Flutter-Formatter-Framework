679f7dc82a5f3d6f4bd21ee921385c65b7f1291c
Qualtagh <Qualtagh@users.noreply.github.com>
2025-05-12 20:12:10 +1200

    feat: extensible context

--- lib/types/text_editing_context.dart
+++ lib/types/text_editing_context.dart
@@ -2,21 +2,32 @@ import 'package:flutter/services.dart';
 import 'package:flutter_formatter_framework/types/change.dart';
 
 class TextEditingContext {
+  final Map<String, dynamic> _data;
+
+  T getValue<T>(String key) => _data[key] as T;
+
+  void setValue<T>(String key, T value) => _data[key] = value;
+
   /// Old text before any editing or formatting.
   /// Corresponds to [oldInput] of [TextInputFormatter.formatEditUpdate].
-  final TextEditingValue old;
+  TextEditingValue get old => getValue<TextEditingValue>('old');
 
   /// Text after raw editing but before any formatting.
-  final TextEditingValue edited;
+  TextEditingValue get edited => getValue<TextEditingValue>('edited');
 
   /// Text after applying all prior formatters in the chain.
   /// Corresponds to [newInput] of [TextInputFormatter.formatEditUpdate].
-  final TextEditingValue preformatted;
+  TextEditingValue get preformatted => getValue<TextEditingValue>('preformatted');
 
   /// The change detected between [old] and [edited].
-  final Change change;
+  Change get change => getValue<Change>('change');
 
-  TextEditingContext({required this.old, required this.edited, required this.preformatted, required this.change});
+  TextEditingContext({
+    required TextEditingValue old,
+    required TextEditingValue edited,
+    required TextEditingValue preformatted,
+    required Change change,
+  }) : _data = {'old': old, 'edited': edited, 'preformatted': preformatted, 'change': change};
 
   TextEditingContext copyWith({TextEditingValue? preformatted}) {
     return TextEditingContext(
